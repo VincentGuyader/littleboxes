e886bf228e4d7d3c574c2df8384c9f5cf98e0446
Vincent <vincent@thinkr.fr>
2016-06-09 20:45:17 +0200

    pour que ca marche meme sans selectionner

--- R/nice.R
+++ R/nice.R
@@ -1,12 +1,12 @@
 dd <- function(x) {
   if (nchar(x) <= 56) {
     base <- (60 - nchar(x) - 2)/2
-    return(paste(c("#", rep(" ", floor(base)), x, rep(" ", ceiling(base)),
-                   "#"), collapse = ""))
+    return(paste(c("#", rep(" ", floor(base)), x, rep(" ",
+      ceiling(base)), "#"), collapse = ""))
   }
   s1 <- strsplit(x, " ")[[1]]
   c(dd(paste(s1[1:floor(length(s1)/2)], collapse = " ")), dd(paste(s1[ceiling(length(s1)/2):length(s1)],
-                                                                   collapse = " ")))
+    collapse = " ")))
 }
 
 
@@ -14,9 +14,9 @@ toutbeau <- function(x, l = 60) {
   # print(x)
   x <- gsub("\n", " ", x)
   x <- gsub("\t", " ", x)
-    x <- gsub("^[# ]+", "", x)
-  x <-    gsub("[# =]+$", " ", x)
-  x <-    gsub(" $", " ", x)
+  x <- gsub("^[# ]+", "", x)
+  x <- gsub("[# =]+$", " ", x)
+  x <- gsub(" $", " ", x)
   res <- paste(rep("#", l), collapse = "")
   res <- c(res, paste(c("#", rep(" ", l - 2), "#"), collapse = ""))
 
@@ -37,13 +37,28 @@ toutbeau <- function(x, l = 60) {
 
 nicetitle <- function() {
   context <- rstudioapi::getActiveDocumentContext()
-
   for (sel in context$selection) {
+    # print(sel)
+    if (sel$text != "") {
 
-    if (sel$text == "")
-      next
 
-    rstudioapi::modifyRange(sel$range, toutbeau(sel$text), context$id)
-    break
+      rstudioapi::modifyRange(sel$range, toutbeau(sel$text),
+        context$id)
+      break
+    } else {
+
+      lign <- context$selection[[1]]$range$start[["row"]]
+      value <- context$contents[lign]
+      range <- structure(list(start = structure(c(lign,
+        1), .Names = c("row", "column"), class = "document_position"),
+        end = structure(c(lign, 77777), .Names = c("row",
+          "column"), class = "document_position")), .Names = c("start",
+        "end"), class = "document_range")
+
+      if (value != "") {
+        rstudioapi::modifyRange(range, toutbeau(value),
+          context$id)
+      }
+    }
   }
 }
